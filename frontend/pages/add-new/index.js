27acdc43ed55f6be57889dde9f00f2a810bf4fe4
singhsduos <neelesh.singh@jalantechnologies.com>
2022-11-05 22:26:13 +0530

    added - addNew component

---
+++ frontend/pages/add-new/index.js
@@ -0,0 +1,168 @@
+import React, { useState, useRef } from "react";
+import { useRouter } from "next/router";
+
+const AddNew = () => {
+    const router = useRouter();
+    
+    
+
+  return (
+    <div className="main__container">
+      <div className="new__invoice">
+        <div className="new__invoice-header">
+          <h3>New Invoice</h3>
+        </div>
+
+        {/* ======== new invoice body ========= */}
+        <div className="new__invoice-body">
+          {/* ======= bill from ========== */}
+          <div className="bill__from">
+            <p className="bill__title">Bill from</p>
+            <div className="form__group">
+              <p>Street Address</p>
+              <input type="text"  />
+            </div>
+
+            <div className="form__group inline__form-group">
+              <div>
+                <p>City</p>
+                <input type="text" />
+              </div>
+
+              <div>
+                <p>Postal Code</p>
+                <input type="text"  />
+              </div>
+
+              <div>
+                <p>Country</p>
+                <input type="text" />
+              </div>
+            </div>
+          </div>
+
+          {/* ========= bill to ========== */}
+          <div className="bill__to">
+            <p className="bill__title">Bill to</p>
+            <div className="form__group">
+              <p>Client Name</p>
+              <input type="text"  />
+            </div>
+
+            <div className="form__group">
+              <p>Client Email</p>
+              <input type="email"  />
+            </div>
+
+            <div className="form__group">
+              <p>Street Address</p>
+              <input type="email"/>
+            </div>
+
+            <div className="form__group inline__form-group">
+              <div>
+                <p>City</p>
+                <input type="text"  />
+              </div>
+
+              <div>
+                <p>Postal Code</p>
+                <input type="text" />
+              </div>
+
+              <div>
+                <p>Country</p>
+                <input type="text"/>
+              </div>
+            </div>
+
+            <div className="form__group inline__form-group">
+              <div className="inline__group">
+                <p>Invoice Date</p>
+                <input type="date" />
+              </div>
+
+              <div className="inline__group">
+                <p>Payment Terms</p>
+                <input type="text"  />
+              </div>
+            </div>
+
+            <div className="form__group">
+              <p>Project Description</p>
+              <input type="text" />
+            </div>
+          </div>
+
+          {/* ========= invoice product items =========*/}
+
+          <div className="invoice__items">
+            <h3>Item List</h3>
+            
+              <div className="item" key={1}>
+                <div className="form__group inline__form-group">
+                  <div>
+                    <p>Item Name</p>
+                    <input
+                      type="text"
+                      name="name"
+                    />
+                  </div>
+
+                  <div>
+                    <p>Qty</p>
+                    <input
+                      type="number"
+                      name="quantity"
+                    />
+                  </div>
+
+                  <div>
+                    <p>Price</p>
+                    <input
+                      type="number"
+                      name="price"
+                    />
+                  </div>
+                  <div>
+                    <p>Total</p>
+                    <h4>item.total</h4>
+                  </div>
+
+                  <button className="edit__btn" >
+                    Delete
+                  </button>
+                </div>
+              </div>
+        
+          </div>
+
+          <button className="add__item-btn">
+            Add New Item
+          </button>
+
+          <div className="new__invoice__btns">
+            <button className="edit__btn" onClick={() => router.push("/")}>
+              Discard
+            </button>
+            <div>
+              <button
+                className="draft__btn"
+              >
+                Save as Draft
+              </button>
+
+              <button
+                className="mark__as-btn"
+              >
+                Send & Save
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddNew;
