6ce26dc0f3f849ebd7276a7f7b569cd7b18e4ee0
singhsduos <neelesh.singh@jalantechnologies.com>
2022-11-05 11:09:22 +0530

    project - setup

---
+++ frontend/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>X Bill</title>
+      </Head>
+    <h1>Hello</h1>
+    </div>
+  )
+}
