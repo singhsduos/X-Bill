2f5e97a86ab8cc0138de1280f3e9a2a69233404f
singhsduos <neelesh.singh@jalantechnologies.com>
2022-11-06 14:15:41 +0530

    added - api fetching for all bills

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,55 +1,91 @@
-import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { MongoClient } from "mongodb";
 
-export default function Home() {
+export default function Home(props) {
   const router = useRouter();
+  const { data } = props;
+
   const navigatePage = () => router.push("/addnew");
 
   return (
-    <>
-      <Head>
-        <title>X Bill</title>
-      </Head>
-      <div className="main__container">
-        <div className="invoice__header">
-          <div className="invoice__header-logo">
-            <h3>Invoices</h3>
-            <p>There are total 8 invoices</p>
-          </div>
-
-          <button className="btn" onClick={navigatePage}>
-            Add New
-          </button>
+    <div className="main__container">
+      <div className="invoice__header">
+        <div className="invoice__header-logo">
+          <h3>Invoices</h3>
+          <p>There are total {data?.size} invoices</p>
         </div>
 
-        <div className="invoice_container">
-          {/* ======= invoice item =========== */}
-          <Link href={`/invoices/12`} passRef key={12}>
+        <button className="btn" onClick={navigatePage}>
+          Add New
+        </button>
+      </div>
+
+      <div className="invoice__container">
+        {/* ======= invoice item =========== */}
+        {data?.map((invoice) => (
+          <Link href={`/invoices/${invoice.id}`} passRef key={invoice.id}>
             <div className="invoice__item">
               <div>
-                <h5 className="invoice__id">RT59K0</h5>
+                <h5 className="invoice__id">
+                  {invoice.id.substr(0, 6).toUpperCase()}
+                </h5>
               </div>
 
               <div>
-                <h6 className="invoice__client">Ramesh</h6>
+                <h6 className="invoice__client">{invoice.clientName}</h6>
               </div>
 
               <div>
-                <p className="invoice__created">27/10/2021</p>
+                <p className="invoice__created">{invoice.createdAt}</p>
               </div>
 
               <div>
-                <h3 className="invoice__total">$4556.2</h3>
+                <h3 className="invoice__total">${invoice.total}</h3>
               </div>
 
               <div>
-                <button className="pending__status">Pending</button>
+                <button
+                  className={`${
+                    invoice.status === "paid"
+                      ? "paid__status"
+                      : invoice.status === "pending"
+                      ? "pending__status"
+                      : "draft__status"
+                  }`}
+                >
+                  {invoice.status}
+                </button>
               </div>
             </div>
           </Link>
-        </div>
+        ))}
       </div>
-    </>
+    </div>
   );
+}
+
+export async function getStaticProps() {
+  const client = await MongoClient.connect(process.env.DB_URI,{ useNewUrlParser: true }
+  );
+
+  const db = client.db();
+  const collection = db.collection("allInvoices");
+
+  const invoices = await collection.find({}).toArray();
+
+  return {
+    props: {
+      data: invoices.map((invoice) => {
+        return {
+          id: invoice._id.toString(),
+          clientName: invoice.clientName,
+          createdAt: invoice.createdAt,
+          total: invoice.total,
+          status: invoice.status,
+        };
+      }),
+    },
+    revalidate: 1,
+  };
 }
