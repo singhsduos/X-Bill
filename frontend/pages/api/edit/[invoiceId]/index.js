780d8bd63f53835d40c8866c4d91939010df2401
singhsduos <neelesh.singh@jalantechnologies.com>
2022-11-06 19:28:39 +0530

    added - editInvoice API

---
+++ frontend/pages/api/edit/[invoiceId]/index.js
@@ -0,0 +1,48 @@
+import { MongoClient, ObjectId } from "mongodb";
+
+async function handler(req, res) {
+  const { invoiceId } = req.query;
+  const client = await MongoClient.connect(process.env.DB_URI,{ useNewUrlParser: true }
+  );
+  const db = client.db();
+  const collection = db.collection("allInvoices");
+
+  if (req.method === "PUT") {
+    await collection.updateOne(
+      {
+        _id: ObjectId(invoiceId),
+      },
+      {
+        $set: {
+          senderAddress: {
+            street: req.body.senderStreet,
+            city: req.body.senderCity,
+            postalCode: req.body.senderPostalCode,
+            country: req.body.senderCountry,
+          },
+          clientName: req.body.clientName,
+          clientEmail: req.body.clientEmail,
+          clientAddress: {
+            street: req.body.clientStreet,
+            city: req.body.clientCity,
+            postalCode: req.body.clientPostalCode,
+            country: req.body.clientCountry,
+          },
+          createdAt: req.body.createdAt,
+          paymentDue: req.body.createdAt,
+          paymentTerms: req.body.paymentTerms,
+          description: req.body.description,
+          status: req.body.status,
+          items: req.body.items,
+          total: req.body.total,
+        },
+      }
+    );
+
+    res.status(200).json({ message: "Invoice updated successfully" });
+  }
+
+  client.close();
+}
+
+export default handler;
