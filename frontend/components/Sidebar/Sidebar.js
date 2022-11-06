d122d01b56e961418611e69fa5aa027d3cf466bd
singhsduos <neelesh.singh@jalantechnologies.com>
2022-11-06 19:27:00 +0530

    Update Sidebar.js

--- frontend/components/Sidebar/Sidebar.js
+++ frontend/components/Sidebar/Sidebar.js
@@ -1,23 +1,23 @@
 import React from "react";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 const Sidebar = () => {
+  const router = useRouter();
+
+  const navigatePage = () => router.push("/");
+
   return (
     <div className="sidebar">
       <div className="sidebar__container">
-        <div className="sidebar__header">
+        <div onClick={navigatePage} className="sidebar__header">
           <div className="sidebar__logo">
             <h2>X-Bill</h2>
           </div>
         </div>
 
         <div className="sidebar__bottom">
-          <Image
-            src="/images/logo.png"
-            alt="avatar"
-            width="50"
-            height="50"
-          />
+          <Image src="/images/logo.png" alt="avatar" width="50" height="50" />
         </div>
       </div>
     </div>
